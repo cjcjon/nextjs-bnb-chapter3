ce26c32129a2ca3028e6c8ef07f95c5de5de00ed
cjcjon <cjcjon30@gmail.com>
2022-01-26 01:07:12 +0900

    feat: Add global header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from "../components/Header"
+import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body {
+          margin: 0;
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default MyApp
